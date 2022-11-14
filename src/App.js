86edef3298cb84e76e10fc9d043d5b0481a218e3
Saagar <Saagar.Ahluwalia@bellevuecollege.edu>
2022-11-13 22:55:17 -0800

    Add routes for login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import * as React from "react";
 import * as Mui from "@mui/material";
 import MediaCard from './Components/Card';
 import ButtonAppBar from "./Components/AppBar";
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './SignIn';
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
           <ButtonAppBar className="Controlcognizant"></ButtonAppBar>
         </header>
         <main>
-          <Mui.Grid container spacing={3} paddingLeft={25}>
+          {/* <Mui.Grid container spacing={3} paddingLeft={25}>
             <Mui.Grid item xs={12} sm={6} md={4}>
               <MediaCard className="" src={require('./Imgs/download.png')} description=""/>
             </Mui.Grid>
@@ -25,7 +27,11 @@ function App() {
             <Mui.Grid item xs={12} sm={6} md={4}>
               <MediaCard className="" src={require('./Imgs/download.png')} description=""/>
             </Mui.Grid>
-          </Mui.Grid>
+          </Mui.Grid> */}
+          <Routes>
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/" element={<></>} />
+          </Routes>
         </main>
         
       </div>
