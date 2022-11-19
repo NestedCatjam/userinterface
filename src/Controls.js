5fb8ac35f59429aafcd568f566450661db700961
dj89harmon <dj89harmon@gmail.com>
2022-11-19 15:17:13 -0800

    Updated Controls.js
    "Add New Control" creates a modal with a form which, on submission, successfully adds a new Control object to the database.

--- src/Controls.js
+++ src/Controls.js
@@ -1,27 +1,56 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { Button, Typography } from '@mui/material';
-
+import {TextField} from '@mui/material';
+import { Button, Modal, Typography } from '@mui/material';
 
 const backend = "";
 
 
-
 export default function Controls() {
 
-  const [rows, setRows] = React.useState([]);
+  const initialPostValues = ["Control_Name", "control_type", "control_subtype", "control_description"];
 
-  const handleDelete = row => {
-    
+  const [rows, setRows] = React.useState([]);
+  const [openModal, setOpenModal] = React.useState(false);
+  const [postValues, setPostValue] = React.useState([]);
+
+  const handleChange = i => {
+    initialPostValues.forEach((x) => {
+      if(i.target.id === x){
+        postValues[x] = i.target.value
+        setPostValue(postValues)
+      }
+    })
+
+    console.log(postValues)
+  }
+
+  const handleDelete = row => {    
     fetch(`${backend}/control/${row.id}`, {method: 'DELETE'}
     ).then(response => {
       response.ok ? setRows(rows.filter(control => control.id !== row.id)) : 
         alert("Error in deletion");
   })
-
     console.log("deleted:", row)
     alert("row deleted:", row)
+  }
+
+  const handlePost = e => {
+    // e.preventDefault();
+
+    const post = `${backend}/control?id=&name=${postValues["Control_Name"]}&description=${postValues["control_description"]}&type=${postValues["control_type"]}&subtype=${postValues["control_subtype"]}`
+    console.log("post:", post)
+    fetch(post, {
+      method: 'POST'
+      // headers: {"Content-Type": "application/json" },
+      // body: JSON.stringify(post)
+    }).then(res => {
+      console.log("res:", res)
+      console.log("new post successfully added")
+    })
+
+    closeModal()
   }
 
   const handleGet = id => {
@@ -33,6 +62,9 @@ export default function Controls() {
       console.log("data returned:", data)
     })
   }
+
+  const openNewControlModal = () => setOpenModal(true);
+  const closeModal = () => setOpenModal(false);
    
   const columns = [
     { field: "id", headerName: "Control ID", width: 150 },
@@ -62,10 +94,12 @@ export default function Controls() {
       });
     }, [])
 
+
     return (
         <>
             <Typography variant="h1" component="h1">Controls</Typography>
-            <Button>Add new control</Button>
+            <Button onClick={() => {openNewControlModal()}}>Add new control</Button>
+            
             <Box sx={{ width: "100%", height: 800 }}>
                 <DataGrid
                     columns={columns}
@@ -73,8 +107,26 @@ export default function Controls() {
                     pageSize={10}
                     rowsPerPageOptions={[10]}
                 />
+            </Box >
 
-            </Box>
+            <Modal
+              open={openModal}
+              onClose={closeModal}
+              aria-labelledby="new-control-modal"
+              aria-describedby="opens modal for creation of a new control"
+              >
+                <Box 
+                component="form"
+                sx={{position:"absolute", width:"50%", height: "40%", marginLeft:"50%", transform:"translate(-50%, 50%)", background:"skyblue"}}>
+                  <h1>New Control</h1>
+                  <TextField id="Control_Name" label="Name" onChange={i => {handleChange(i)}} required></TextField>
+                  <TextField id="control_type" label="Type" onChange={i => {handleChange(i)}} required></TextField>
+                  <TextField id="control_subtype" label="Subtype" onChange={i => {handleChange(i)}} required></TextField>
+                  <TextField id="control_description" label="Description" onChange={i => {handleChange(i)}} required></TextField>
+                  
+                  <Button onClick={e => handlePost(e)}>Submit</Button>
+                </Box>
+              </Modal>
         </>
     );
 };
